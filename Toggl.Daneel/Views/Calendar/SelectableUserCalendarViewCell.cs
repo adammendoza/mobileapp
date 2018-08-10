b3b62d699c3ab76c94ed574bae4b63c2a7786842
Arturs Eimanis <arturseimanis@gmail.com>
2018-08-10 12:55:53 +0300

    :art: Add SelectableUserCalendarViewCell

---
+++ Toggl.Daneel/Views/Calendar/SelectableUserCalendarViewCell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using Toggl.Daneel.Extensions;
+using Toggl.Foundation.MvvmCross.ViewModels.Selectable;
+using UIKit;
+
+namespace Toggl.Daneel.Views.Calendar
+{
+    public sealed partial class SelectableUserCalendarViewCell : MvxTableViewCell
+    {
+        public static readonly NSString Key = new NSString(nameof(SelectableUserCalendarViewCell));
+        public static readonly UINib Nib;
+
+        static SelectableUserCalendarViewCell()
+        {
+            Nib = UINib.FromName(nameof(SelectableUserCalendarViewCell), NSBundle.MainBundle);
+        }
+
+        protected SelectableUserCalendarViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            prepareViews();
+
+            this.DelayBind(() =>
+            {
+                var bindingSet = this.CreateBindingSet<SelectableUserCalendarViewCell, SelectableCalendarViewModel>();
+
+                bindingSet.Bind(CalendarNameLabel).To(vm => vm.Name);
+                bindingSet.Bind(IsSelectedSwitch).To(vm => vm.Selected);
+
+                bindingSet.Apply();
+            });
+        }
+
+        private void prepareViews()
+        {
+            IsSelectedSwitch.Resize();
+        }
+    }
+}
