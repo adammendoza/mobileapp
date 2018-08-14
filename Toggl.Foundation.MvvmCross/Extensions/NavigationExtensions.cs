73e480d6456f4b2d04171154ba66ff88686eef21
daividssilverio <daivid.silverio@gmail.com>
2018-08-14 15:28:13 -0400

    :sparkles: Add ForkNavigate extension

---
+++ Toggl.Foundation.MvvmCross/Extensions/NavigationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using MvvmCross;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace Toggl.Foundation.Extensions
+{
+    public static class NavigationExtensions
+    {
+        public static Task ForkNavigate<TDaneelViewModel, TGiskardViewModel>(this IMvxNavigationService navigationService)
+            where TDaneelViewModel : IMvxViewModel
+            where TGiskardViewModel : IMvxViewModel
+        {
+            var platformInfo = Mvx.Resolve<PlatformInfo>();
+
+            if (platformInfo.Platform == Platform.Daneel)
+            {
+                return navigationService.Navigate<TDaneelViewModel>();
+            }
+
+            return navigationService.Navigate<TGiskardViewModel>();
+        }
+    }
+}
